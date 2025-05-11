554d735e175ffd6d57914e6f069e773501c3fe69
dumbdumb666 <taufikpratama095@gmail.com>
2025-05-11 11:42:01 +0700

    Update test.py

--- test.py
+++ test.py
@@ -23,14 +23,3 @@ pilihan = input("Masukkan pilihan (1/2/3/4): ")
 
 angka1 = float(input("Masukkan angka pertama: "))
 angka2 = float(input("Masukkan angka kedua: "))
-
-if pilihan == '1':
-    print("Hasil:", tambah(angka1, angka2))
-elif pilihan == '2':
-    print("Hasil:", kurang(angka1, angka2))
-elif pilihan == '3':
-    print("Hasil:", kali(angka1, angka2))
-elif pilihan == '4':
-    print("Hasil:", bagi(angka1, angka2))
-else:
-    print("Pilihan tidak valid.")
